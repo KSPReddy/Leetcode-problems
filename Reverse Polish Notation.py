4a320206f169403a4e33a428eff2f21dc5838b59
Sai Praneeth Reddy Kethiri <saipraneethreddykethiri@Sais-MacBook-Air-2.local>
2024-01-25 17:10:22 -0500

    Reverse Polish Notation Problem

---
+++ Reverse Polish Notation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for t in tokens:
+            if t not in "+-*/":
+                stack.append(int(t))
+            else:
+                r, l = stack.pop(), stack.pop()
+            if t == "+":
+                stack.append(l+r)
+            elif t == "-":
+                stack.append(l-r)
+            elif t == "*":
+                stack.append(l*r)
+            elif t == '/':
+                stack.append(int(float(l)/r))
+        return stack.pop()
